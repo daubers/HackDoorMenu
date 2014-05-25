16c91d8cb8453448f6e53ff13c9083b8b81b9d73
Matthew Daubney <matt@daubers.co.uk>
2014-05-25 15:20:23 +0100

    Initial commit

---
+++ HackDoorMenu/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+
+
+from HackDoor.views import main
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'HackDoorMenu.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', main),
+)
+
